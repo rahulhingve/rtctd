53f352844134c0dea18fde26caf746ed21dcfb3a
unknown <rahulpawar2001.rp@gmail.com>
2024-06-26 22:05:58 +0530

    testing protected endpoint

--- apps/web/app/dashboard/page.tsx
+++ apps/web/app/dashboard/page.tsx
@@ -1,5 +1,12 @@
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { options } from "../api/auth/[...nextauth]/options";
 
-export default function() {
+export default async function() {
+    const session = await getServerSession(options)
+   if(!session){
+    redirect("/api/auth/signin");
+   }
     return <div>
         Dashboard
     </div>
