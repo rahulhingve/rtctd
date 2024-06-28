dbd6df46652859d4750926a6a3d81c88469db015
unknown <rahulpawar2001.rp@gmail.com>
2024-06-28 12:22:49 +0530

    updated codeeditor

--- apps/web/app/components/CodeEditor.tsx
+++ apps/web/app/components/CodeEditor.tsx
@@ -3,6 +3,7 @@ import React, { useRef, useState } from 'react'
 import Editor from '@monaco-editor/react';
 import LanguageSelector from './LanguageSelector';
 import { CODE_SNIPPETS } from '../../constants';
+import Output from './Output';
 
 const CodeEditor = () => {
   const editorRef = useRef()
@@ -14,20 +15,28 @@ const CodeEditor = () => {
     editor.focus()
   }
 
-  const onSelect = (language:any)=>{
+  const onSelect = (language:any) => {
     setLanguage(language)
+    
     setValue(
-      CODE_SNIPPETS[language] 
+
+      CODE_SNIPPETS[language]
     )
   }
 
   return (
-    <div>
-     <LanguageSelector language={language} onSelect={onSelect}/>
-      <div>
-      
-        <Editor  theme='vs-dark' height="90vh" language={language} defaultValue="// some comment" onMount={onMount} value={value} onChange={(value: any) => setValue(value)} />;
+    <div className=' flex bg-[#333]'>
+
+     
+
+      <div className='w-[60%] h-screen ' >
+      <LanguageSelector language={language} onSelect={onSelect} />
+        <Editor theme='vs-dark' height="95vh" language={language} defaultValue={CODE_SNIPPETS[language]} onMount={onMount} value={value} onChange={(value: any) => setValue(value)} />
       </div>
+      <div className='w-[40%] h-screen bg-[#333]'>
+        <Output editorRef={editorRef} language={language} />
+      </div>
+
 
 
     </div>
