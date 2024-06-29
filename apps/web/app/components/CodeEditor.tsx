ed4d81d901523f7c8632ad4283c9f70a4d7e1d27
unknown <rahulpawar2001.rp@gmail.com>
2024-06-29 19:38:27 +0530

    Intrigated WebRTC for Real time Collaboration

--- apps/web/app/components/CodeEditor.tsx
+++ apps/web/app/components/CodeEditor.tsx
@@ -4,20 +4,31 @@ import Editor from '@monaco-editor/react';
 import LanguageSelector from './LanguageSelector';
 import { CODE_SNIPPETS } from '../../constants';
 import Output from './Output';
+import * as Y from "yjs";
+import { WebrtcProvider } from 'y-webrtc';
+import { MonacoBinding } from 'y-monaco';
 
 const CodeEditor = () => {
   const editorRef = useRef()
   const [value, setValue] = useState('')
   const [language, setLanguage] = useState('javascript')
 
-  const onMount = (editor: any) => {
+  const onMount = (editor: any, monaco: any) => {
     editorRef.current = editor;
     editor.focus()
+
+    // Yjs to handle all steps here
+    const doc = new Y.Doc();
+    const provider = new WebrtcProvider("room", doc);
+    const type = doc.getText("monaco");
+    const binding = new MonacoBinding(type, editorRef.current?.getModel(),new Set([editorRef.current]),provider.awareness);
+
+console.log(provider.awareness);
   }
 
-  const onSelect = (language:any) => {
+  const onSelect = (language: any) => {
     setLanguage(language)
-    
+
     setValue(
 
       CODE_SNIPPETS[language]
@@ -27,10 +38,10 @@ const CodeEditor = () => {
   return (
     <div className=' flex bg-[#333]'>
 
-     
+
 
       <div className='w-[60%] h-screen ' >
-      <LanguageSelector language={language} onSelect={onSelect} />
+        <LanguageSelector language={language} onSelect={onSelect} />
         <Editor theme='vs-dark' height="95vh" language={language} defaultValue={CODE_SNIPPETS[language]} onMount={onMount} value={value} onChange={(value: any) => setValue(value)} />
       </div>
       <div className='w-[40%] h-screen bg-[#333]'>
