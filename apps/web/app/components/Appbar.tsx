b572d1f4e5554f6c4a1612a132915a7133af0020
unknown <rahulpawar2001.rp@gmail.com>
2024-07-04 09:30:31 +0530

    session based Signout button

--- apps/web/app/components/Appbar.tsx
+++ apps/web/app/components/Appbar.tsx
@@ -3,9 +3,12 @@ import React from 'react'
 import Image from "next/image"
 import { redirect } from 'next/navigation'
 import Link from 'next/link'
-import { signOut } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 
 const Appbar = () => {
+
+
+  const {data: session}= useSession();
   return (
    
 
@@ -39,7 +42,7 @@ const Appbar = () => {
           <a href="#" className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Contact</a>
         </li>
         <li>
-          <button onClick={()=>signOut({ callbackUrl: 'http://localhost:3000/dashboard' })} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Signout</button>
+          {session? <button onClick={()=>signOut({ callbackUrl: 'http://localhost:3000/dashboard' })} className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Signout</button> : null}
           
         </li>
       </ul>
